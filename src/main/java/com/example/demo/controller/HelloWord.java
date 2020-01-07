95de626f63a10ffb31804882876d97dceecf362b
zhangkanglei <1017096373@qq.com>
2020-01-07 10:06:30 +0800

    配置swagger

--- src/main/java/com/example/demo/controller/HelloWord.java
+++ src/main/java/com/example/demo/controller/HelloWord.java
@@ -1,14 +1,20 @@
 package com.example.demo.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by 张康磊 on 2017/9/3.
  */
 @RestController
+@Api(tags = "测试相关接口")
+@RequestMapping("/test")
 public class HelloWord {
 
-    @RequestMapping("hello")
+    @GetMapping("/hello")
+    @ApiOperation("添加用户的接口")
     public String say(){
         return "hello spring boot";
     }
